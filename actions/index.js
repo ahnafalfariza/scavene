84c07ea2a93d87fdade111517ac0ae4119016553
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2024-10-08 11:38:55 +0700

    feat: more updates to the action script

--- actions/index.js
+++ actions/index.js
@@ -1,10 +1,28 @@
 const exec = require('@actions/exec')
 const github = require('@actions/github')
+const core = require('@actions/core')
+const { table } = require('table');
 
 function displayTable(data) {
+  console.log("\n\n\n")
+
   for (const file of data) {
-    console.log(file.file_path)
-    console.table(file.vulnerabilities)
+    const tableData = [
+      ['Severity', 'Description', 'Line', 'Code Snippet'], // Header row
+      ...file.vulnerabilities.map(item => [
+        item.severity_level,
+        item.severity_description,
+        item.line_number,
+        item.code_snippet,
+      ])
+    ];
+
+    console.log(`File path: ${file.file_path}`)
+    console.log(table(tableData, {
+      columns: {
+        1: { width: 50 },
+      }
+    }));
     console.log('\n')
   }
 }
@@ -24,7 +42,10 @@ async function main() {
   const parsedData = JSON.parse(myOutput)
   displayTable(parsedData)
 
-  await github.getOctokit(process.env.GITHUB_TOKEN).rest.issues.createComment({
+  const myToken = core.getInput('myToken');
+  const octokit = github.getOctokit(myToken)
+
+  await octokit.rest.issues.createComment({
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
     issue_number: github.context.issue.number,
